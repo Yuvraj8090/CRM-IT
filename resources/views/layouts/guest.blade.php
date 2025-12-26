70c2d5614364fbe21c9914c9fd1cb77c8e805733
Yuvraj8090 <135937985+Yuvraj8090@users.noreply.github.com>
2025-12-27 00:06:44 +0530

    s

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ allsettings('site.title') }}</title>
 
     <!-- Fonts -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
